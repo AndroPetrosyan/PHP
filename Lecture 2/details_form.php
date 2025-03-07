5a8714ff060cd74fe5cc149912cbac054667ff95
AndroPetrosyan <76426753+AndroPetrosyan@users.noreply.github.com>
2025-03-07 10:29:07 +0400

    Create details_form.php

---
+++ Lecture 2/details_form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Lecture</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body style="font-family:Arial, Helvetica, sans-serif;">
+    <br><br><br><br><br><br><br><br><br>
+    <div class="container">
+        <form action="details.php" method="get">
+            <h1>Enter Details</h1>
+            <p>Name</p>
+            <input type="text" name="name" placeholder="Enter your name">
+            <p>Surname</p>
+            <input type="text" name="surname" placeholder="Enter your surname">
+            <p>Gross Income</p>
+            <input type="text" name="gross" placeholder="Enter your gross income">
+            <p>Tax</p>
+            <input type="text" name="tax" placeholder="Enter your tax">
+            <br><br><br>
+            <button>Submit</button>
+        </form>
+
+    </div>
+</body>
+
+</html>
